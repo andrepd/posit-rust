52df6aebb0e598080d16c54e7d368a1072388239
andrepd <andrepd@protonmail.com>
2025-07-05 22:19:23 +0100

    Add type aliases for standard posits

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,19 @@ mod underlying;
 
 pub use posit::Posit;
 pub use underlying::Int;
+
+/// Standard-compliant 8-bit posit.
+#[allow(non_camel_case_types)]
+pub type p8 = Posit<8, 2, i8>;
+
+/// Standard-compliant 16-bit posit.
+#[allow(non_camel_case_types)]
+pub type p16 = Posit<16, 2, i16>;
+
+/// Standard-compliant 32-bit posit.
+#[allow(non_camel_case_types)]
+pub type p32 = Posit<32, 2, i32>;
+
+/// Standard-compliant 64-bit posit.
+#[allow(non_camel_case_types)]
+pub type p64 = Posit<64, 2, i64>;
