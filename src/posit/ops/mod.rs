25c8e4876a573adbcbc7b8ecf53f2f8474e8473f
andrepd <andrepd@protonmail.com>
2025-09-19 17:01:40 +0100

    Cut boilerplate by moving the tests for add, sub, mul, div into a macro
    (They're exactly the same, it's literally copy-paste and change `+` to
    `-` to `*` to `/`).

--- src/posit/ops/mod.rs
+++ src/posit/ops/mod.rs
@@ -61,3 +61,91 @@ macro_rules! mk_ops {
 
 pub(crate) use mk_ops;
 
+/// Macro for instantating the suite of tests for a binary operator of posits.
+macro_rules! mk_tests {
+  ($op:tt, $op_assign:tt) => {
+    use crate::Posit;
+    use malachite::rational::Rational;
+    use proptest::prelude::*;
+
+    #[allow(dead_code)]
+    fn ops() {
+      let mut a = crate::p32::ONE;
+      let mut b = crate::p32::MINUS_ONE;
+      let _ = a $op b;
+      let _ = &a $op b;
+      let _ = a $op &b;
+      let _ = &a $op &b;
+      a $op_assign b;
+      b $op_assign &a;
+    }
+
+    /// Aux function: check that `a $op b` is rounded correctly.
+    fn is_correct_rounded<const N: u32, const ES: u32, Int: crate::Int>(
+      a: Posit<N, ES, Int>,
+      b: Posit<N, ES, Int>,
+    ) -> bool
+    where
+      Rational: TryFrom<Posit<N, ES, Int>, Error = super::rational::IsNaR>,
+    {
+      let posit = a $op b;
+      if let (Ok(a), Ok(b)) = (Rational::try_from(a), Rational::try_from(b)) {
+        if stringify!($op) == "/" && b == Rational::from(0) {
+          return posit == Posit::NAR
+        }
+        let exact = a $op b;
+        super::rational::is_correct_rounded(exact, posit)
+      } else {
+        posit == Posit::NAR
+      }
+    }
+
+    macro_rules! test_exhaustive {
+      ($name:ident, $posit:ty) => {
+        #[test]
+        fn $name() {
+          for a in <$posit>::cases_exhaustive_all() {
+            for b in <$posit>::cases_exhaustive_all() {
+              assert!(is_correct_rounded(a, b), "{:?} ⋅ {:?}", a, b)
+            }
+          }
+        }
+      };
+    }
+
+    macro_rules! test_proptest {
+      ($name:ident, $posit:ty) => {
+        proptest!{
+          #![proptest_config(ProptestConfig::with_cases(crate::PROPTEST_CASES))]
+          #[test]
+          fn $name(
+            a in <$posit>::cases_proptest_all(),
+            b in <$posit>::cases_proptest_all(),
+          ) {
+            assert!(is_correct_rounded(a, b), "{:?} ⋅ {:?}", a, b)
+          }
+        }
+      };
+    }
+
+    test_exhaustive!{posit_10_0_exhaustive, Posit::<10, 0, i16>}
+    test_exhaustive!{posit_10_1_exhaustive, Posit::<10, 1, i16>}
+    test_exhaustive!{posit_10_2_exhaustive, Posit::<10, 2, i16>}
+    test_exhaustive!{posit_10_3_exhaustive, Posit::<10, 3, i16>}
+
+    test_exhaustive!{posit_8_0_exhaustive, Posit::<8, 0, i8>}
+
+    // Above ~10 bits = 2^20 operations, it's infeasible to test binary operations exhaustively,
+    // especially when not in a release build (in that case, we maybe can go to ~16 bits = 2^32).
+    test_exhaustive!{p8_exhaustive, crate::p8}
+    test_proptest!{p16_proptest, crate::p16}
+    test_proptest!{p32_proptest, crate::p32}
+    test_proptest!{p64_proptest, crate::p64}
+
+    test_exhaustive!{posit_3_0_exhaustive, Posit::<3, 0, i8>}
+    test_exhaustive!{posit_4_0_exhaustive, Posit::<4, 0, i8>}
+    test_exhaustive!{posit_4_1_exhaustive, Posit::<4, 1, i8>}
+  }
+}
+
+pub(crate) use mk_tests;
