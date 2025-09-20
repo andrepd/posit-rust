0611085c583adc8c4fda1fc9b6b041fc6fe37b17
andrepd <andrepd@protonmail.com>
2025-09-20 14:47:11 +0100

    Float conversions: test subnormals, add more test cases

--- src/posit/convert/float.rs
+++ src/posit/convert/float.rs
@@ -195,6 +195,20 @@ mod tests {
       }
 
       #[test]
+      fn min() {
+        if const { <$posit>::MAX_EXP as i64 <= 127 } {
+          assert_eq!(<$posit>::round_from(<$float>::MIN), <$posit>::MIN)
+        }
+      }
+
+      #[test]
+      fn max() {
+        if const { <$posit>::MAX_EXP as i64 <= 127 } {
+          assert_eq!(<$posit>::round_from(<$float>::MAX), <$posit>::MAX)
+        }
+      }
+
+      #[test]
       fn min_positive() {
         if const { <$posit>::MAX_EXP as i64 <= 127 } {
           assert_eq!(<$posit>::round_from(<$float>::MIN_POSITIVE), <$posit>::MIN_POSITIVE)
@@ -205,6 +219,20 @@ mod tests {
       fn max_negative() {
         if const { <$posit>::MAX_EXP as i64 <= 127 } {
           assert_eq!(<$posit>::round_from(-<$float>::MIN_POSITIVE), <$posit>::MAX_NEGATIVE)
+        }
+      }
+
+      #[test]
+      fn subnormal_positive() {
+        if const { <$posit>::MAX_EXP as i64 <= 127 } {
+          assert_eq!(<$posit>::round_from(<$float>::from_bits(1)), <$posit>::MIN_POSITIVE)
+        }
+      }
+
+      #[test]
+      fn subnormal_negative() {
+        if const { <$posit>::MAX_EXP as i64 <= 127 } {
+          assert_eq!(<$posit>::round_from(-<$float>::from_bits(1)), <$posit>::MAX_NEGATIVE)
         }
       }
 
@@ -235,6 +263,11 @@ mod tests {
     mod posit_10_1 { make_tests!{f64, Posit::<10, 1, i16>} }
     mod posit_10_2 { make_tests!{f64, Posit::<10, 2, i16>} }
     mod posit_10_3 { make_tests!{f64, Posit::<10, 3, i16>} }
+    mod posit_20_4 { make_tests!{f64, Posit::<20, 4, i32>} }
+
+    mod posit_3_0 { make_tests!{f64, Posit::<3, 0, i8>} }
+    mod posit_4_0 { make_tests!{f64, Posit::<4, 0, i8>} }
+    mod posit_4_1 { make_tests!{f64, Posit::<4, 1, i8>} }
   }
 
   mod f32 {
@@ -250,5 +283,10 @@ mod tests {
     mod posit_10_1 { make_tests!{f32, Posit::<10, 1, i16>} }
     mod posit_10_2 { make_tests!{f32, Posit::<10, 2, i16>} }
     mod posit_10_3 { make_tests!{f32, Posit::<10, 3, i16>} }
+    mod posit_20_4 { make_tests!{f32, Posit::<20, 4, i32>} }
+
+    mod posit_3_0 { make_tests!{f32, Posit::<3, 0, i8>} }
+    mod posit_4_0 { make_tests!{f32, Posit::<4, 0, i8>} }
+    mod posit_4_1 { make_tests!{f32, Posit::<4, 1, i8>} }
   }
 }
