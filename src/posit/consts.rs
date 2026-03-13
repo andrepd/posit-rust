6533e1fe68fab16441265c450c2a72500f3595d5
andrepd <andrepd@protonmail.com>
2026-03-13 16:36:22 +0000

    Add Posit::INT_MAX, the largest consecutive integer posit of that type

--- src/posit/consts.rs
+++ src/posit/consts.rs
@@ -50,6 +50,71 @@ impl<
   // Represented by the bit pattern `0b1111...1`.
   pub const MAX_NEGATIVE: Self = Self(const_as(-1));
 
+  /// The largest *consecutive* integer value, that is: integers in the range `0 ..= Self::INT_MAX`
+  /// are exactly representable as a posit of this type.
+  ///
+  /// Standard: "[**pIntMax**](https://posithub.org/docs/posit_standard-2.pdf#subsection.3.2)".
+  ///
+  /// # Example
+  ///
+  /// ```
+  /// # use fast_posit::*;
+  /// assert_eq!(p8::INT_MAX, 1 << 4);
+  /// assert_eq!(p16::INT_MAX, 1 << 10);
+  /// assert_eq!(p32::INT_MAX, 1 << 23);
+  /// assert_eq!(p64::INT_MAX, 1 << 48);
+  /// ```
+  pub const INT_MAX: Int = Self::int_max();
+
+  const fn int_max() -> Int {
+    // If a (positive) power of two is representable as a posit, it looks like this:
+    //
+    //   0b0_11…10_ee_000000…
+    //
+    // A 0 sign bit, a positive `regime` represented by a sequence of `regime + 1` 1s terminated by
+    // a 0, then `ES` exponent bits, then 0s all the way through the end for the fraction bits.
+    //
+    // To figure out what's the maximum integer such that all numbers from 0 to that integer are
+    // exactly representable as a posit, let's work by induction by thinking like this: the number
+    // `1` is always representable as a posit. Then, if a positive power of two `1 << power` is
+    // representable as a posit, the next number is representable if
+    //
+    //   0b0_11…10_ee_000…001000…
+    //
+    // is representable as a posit, where there are `power - 1` 0s in the fraction before the 1.
+    //
+    // If this is the case, then all the numbers up to the next power of two are also representable
+    //
+    //   0b0_11…10_ee_000…010000…
+    //   0b0_11…10_ee_000…011000…
+    //   0b0_11…10_ee_000…100000…
+    //   …
+    //
+    // But we also know that the number of fraction bits is `N - 1 - (regime + 1) - 1 - ES`.
+    // Therefore, if this number is smaller than `power`, then we cannot represent the integer
+    // after `1 << power` as a posit without rounding, meaning `1 << power` is the largest
+    // consecutive integer representable as this posit.
+    //
+    // So we just iterate through powers of two until we find the biggest representable `power`;
+    // the answer is `1 << power`.
+    let mut power = 0;
+    while power < Int::BITS {
+      let regime = power >> ES;
+      // If not even enough bits to represent the regime and exponent fields, this is `INT_MAX`.
+      if Self::BITS < regime + ES + 3 {
+        return const_as::<i128, Int>(1 << power)
+      }
+      // Otherwise we have `fraction_bits` left over, if we cannot represent the next integer, this
+      // is `INT_MAX`.
+      let fraction_bits = Self::BITS - regime - ES - 3;
+      if fraction_bits < power {
+        return const_as::<i128, Int>(1 << power)
+      }
+      power += 1
+    }
+    unreachable!()
+  }
+
   /// The maximum exponent; [`Self::MAX`] = 2 <sup>[`Self::MAX_EXP`]</sup>. Equal to `-MIN_EXP`.
   pub const MAX_EXP: Int = {
     let max_regime = N as i128 - 2;
@@ -70,25 +135,6 @@ impl<
 #[allow(overflowing_literals)]
 mod tests {
   use super::*;
-
-  /*#[test]
-  fn zero() {
-    assert_eq!(Posit::<8,   2, i8  >::ZERO.to_bits(), 0);
-    assert_eq!(Posit::<16,  2, i16 >::ZERO.to_bits(), 0);
-    assert_eq!(Posit::<32,  2, i32 >::ZERO.to_bits(), 0);
-    assert_eq!(Posit::<64,  2, i64 >::ZERO.to_bits(), 0);
-    assert_eq!(Posit::<128, 2, i128>::ZERO.to_bits(), 0);
-
-    assert_eq!(Posit::<8,   0, i8  >::ZERO.to_bits(), 0);
-    assert_eq!(Posit::<16,  1, i16 >::ZERO.to_bits(), 0);
-    assert_eq!(Posit::<32,  2, i32 >::ZERO.to_bits(), 0);
-    assert_eq!(Posit::<64,  3, i64 >::ZERO.to_bits(), 0);
-    assert_eq!(Posit::<128, 4, i128>::ZERO.to_bits(), 0);
-
-    assert_eq!(Posit::<6, 1, i8>::ZERO.to_bits(), 0);
-    assert_eq!(Posit::<10, 2, i64>::ZERO.to_bits(), 0);
-    assert_eq!(Posit::<32, 2, i64>::ZERO.to_bits(), 0);
-  }*/
 
   use malachite::rational::Rational;
 
@@ -218,6 +264,38 @@ mod tests {
       Rational::try_from(Posit::<10, 1, i16>::MINUS_ONE),
       Ok(-Rational::from(1)),
     );
+  }
+
+  #[test]
+  fn int_max() {
+    fn exhaustive<const N: u32, const ES: u32, Int: crate::Int>() {
+      use crate::RoundInto;
+      // All numbers from 0 to INT_MAX round-trip losslessly to posit
+      let int_max: i32 = const_as(Posit::<N, ES, Int>::INT_MAX);
+      for int in 0 ..= int_max {
+        let posit: Posit::<N, ES, Int> = int.round_into();
+        let re_int: i32 = posit.round_into();
+        assert_eq!(int, re_int)
+      }
+      // The number immediately after doesn't
+      let int_invalid: i32 = int_max + 1;
+      let posit_invalid: Posit::<N, ES, Int> = int_invalid.round_into();
+      let re_int_invalid: i32 = posit_invalid.round_into();
+      assert_ne!(int_invalid, re_int_invalid)
+    }
+
+    exhaustive::<10, 0, i16>();
+    exhaustive::<10, 1, i16>();
+    exhaustive::<10, 2, i16>();
+    exhaustive::<10, 3, i16>();
+    exhaustive::<8, 0, i8>();
+    exhaustive::<8, 2, i8>();
+    exhaustive::<16, 2, i16>();
+    exhaustive::<32, 2, i32>();
+    exhaustive::<32, 2, i32>();
+    exhaustive::<3, 0, i8>();
+    exhaustive::<4, 0, i8>();
+    exhaustive::<4, 1, i8>();
   }
 
   /// Aux function: the max value of an n-bit posit with 2-bit exponent (as per the standard).
